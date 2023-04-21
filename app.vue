34fdfd97d812f5bcb8170a5c6508f129e837bae7
Julie <julie.albini@edu.univ-fcomte.fr>
2023-04-21 09:27:52 +0200

    feat(saisons) ; intégration de la fonction gérant les couleurs selon les saisons

--- app.vue
+++ app.vue
@@ -1,20 +1,71 @@
 <template>
   <div>
-    <e-bouton couleur="red">Commencer l’expérience</e-bouton>
-    <br />
-    <br />
-    <e-bouton couleur="red" size="small">Commencer l’expérience</e-bouton>
-    <br />
-    <br />
-    <e-bouton couleur="summer-primary-color" dimension
-      >Commencer l'expérience</e-bouton
-    >
+    <p :style="{ color: primaryColor }">hguafujvqefbbuefj</p>
+    <p :style="{ color: secondaryColor }">hguafujvqefbbuefj</p>
   </div>
 </template>
 
 <style lang="scss">
 .couleur {
-  color: red;
-  font-family: $primary-font-familly;
+  background-color: red;
 }
 </style>
+
+<script>
+import { ref, onMounted, onUnmounted } from "vue";
+
+export default {
+  setup() {
+    const primaryColor = ref("");
+    const secondaryColor = ref("");
+
+    const setColor = () => {
+      const date = new Date();
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+
+      if (
+        (month === 12 && day >= 21) ||
+        month === 1 ||
+        (month === 2 && day <= 20)
+      ) {
+        primaryColor.value = "#FFFFFF";
+        secondaryColor.value = "#4359AA";
+      } else if (
+        (month === 3 && day >= 21) ||
+        month === 4 ||
+        (month === 5 && day <= 20)
+      ) {
+        primaryColor.value = "#2E3E1A";
+        secondaryColor.value = "#D398EF";
+      } else if (
+        (month === 6 && day >= 21) ||
+        month === 7 ||
+        (month === 8 && day <= 20)
+      ) {
+        primaryColor.value = "#D09C14";
+        secondaryColor.value = "#FFFFFF";
+      } else if (
+        (month === 9 && day >= 21) ||
+        month === 10 ||
+        (month === 11 && day <= 20)
+      ) {
+        primaryColor.value = "#391B0A";
+        secondaryColor.value = "#DE8706";
+      }
+    };
+
+    onMounted(() => {
+      setColor();
+    });
+
+    onUnmounted(() => {
+      setColor();
+    });
+    return {
+      primaryColor,
+      secondaryColor,
+    };
+  },
+};
+</script>
