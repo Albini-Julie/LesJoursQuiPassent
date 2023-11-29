b59e05f56889cbaa142b8a4a0cf21767489ce928
Julie <julie.albini@edu.univ-fcomte.fr>
2023-11-29 13:09:26 +0100

    feat(font) : ajout de la font Urbaniste

--- app.vue
+++ app.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <p class="couleur">Bonjour</p>
+    <e-bouton>Commencer l’expérience</e-bouton>
+    <br />
+    <br />
+    <e-bouton couleur="$winter-secondary-color" size="small"
+      >Commencer l’expérience</e-bouton
+    >
+    <br />
+    <br />
+    <e-bouton dimension>Commencer l'expérience</e-bouton>
   </div>
 </template>
 
 <style lang="scss">
 .couleur {
   color: red;
+  font-family: $primary-font-familly;
 }
 </style>
