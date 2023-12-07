20b12abfe4c95e62067875949d453312af83733f
Julie <julie.albini@edu.univ-fcomte.fr>
2023-12-07 11:41:23 +0100

    feat(insert) : début de l'intégration de la page Insert

--- pages/insert.vue
+++ pages/insert.vue
@@ -1,3 +1,66 @@
-<template></template>
+<template>
+  <c-header :couleur="secondaryColor" croix="false" filtre="false" />
+</template>
 
-<script></script>
+<script>
+import { ref, onMounted, onUnmounted } from "vue";
+
+export default {
+  setup() {
+    const primaryColor = ref("");
+    const secondaryColor = ref("");
+
+    const setColor = () => {
+      const date = new Date();
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+
+      if (
+        (month === 12 && day >= 21) ||
+        month === 1 ||
+        month === 2 ||
+        (month === 3 && day < 21)
+      ) {
+        primaryColor.value = "#FFFFFF";
+        secondaryColor.value = "#4359AA";
+      } else if (
+        (month === 3 && day >= 21) ||
+        month === 4 ||
+        (month === 5 && day <= 20) ||
+        (month === 6 && day < 21)
+      ) {
+        primaryColor.value = "#2E3E1A";
+        secondaryColor.value = "#D398EF";
+      } else if (
+        (month === 6 && day >= 21) ||
+        month === 7 ||
+        month === 8 ||
+        (month === 9 && day < 21)
+      ) {
+        primaryColor.value = "#D09C14";
+        secondaryColor.value = "#FFFFFF";
+      } else if (
+        (month === 9 && day >= 21) ||
+        month === 10 ||
+        month === 11 ||
+        (month === 12 && day < 21)
+      ) {
+        primaryColor.value = "#391B0A";
+        secondaryColor.value = "#DE8706";
+      }
+    };
+
+    onMounted(() => {
+      setColor();
+    });
+
+    onUnmounted(() => {
+      setColor();
+    });
+    return {
+      primaryColor,
+      secondaryColor,
+    };
+  },
+};
+</script>
